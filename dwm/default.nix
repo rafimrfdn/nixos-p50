184fed59d08082b16c58cc546eef5f15d75f61f6
rafimrfdn <uphy.mks@gmail.com>
2024-03-16 23:18:50 +0800

    tambah dwm inplacerotate

--- dwm/default.nix
+++ dwm/default.nix
@@ -4,45 +4,45 @@
   # DWM
   services.xserver.windowManager.dwm.enable = true;
   services.xserver.displayManager.defaultSession = "none+dwm";
- #  services.dwm-status.enable = true;
- #  services.dwm-status.order = [ "backlight" "time" "battery"  ];
- #  services.dwm-status.extraConfig = ''
- #  	debug = false
-	# separator = "    "
-	# 
-	# [audio]
-	# control = "Master"
-	# mute = "ﱝ"
-	# template = "{ICO} {VOL}%"
-	# icons = ["奄", "奔", "墳"]
-	# 
-	# [backlight]
-	# device = "intel_backlight"
-	# template = "{ICO} {BL}%"
-	# icons = ["", "", ""]
-	#
-	# [battery]
-	# charging = ""
-	# discharging = ""
-	# enable_notifier = true
-	# no_battery = ""
-	# notifier_critical = 10
-	# notifier_levels = [2, 5, 10, 15, 20]
-	# separator = " · "
-	# icons = ["", "", "", "", "", "", "", "", "", "", ""]
-	# 
-	# [cpu_load]
-	# template = "{CL1} {CL5} {CL15}"
-	# update_interval = 20
-	# 
-	# [network]
-	# no_value = "NA"
-	# template = "{IPv4} · {IPv6} · {ESSID}"
-	# 
-	# [time]
-	# format = "%d-%m-%Y %H:%M"
-	# update_seconds = false
- #  '';
+  services.dwm-status.enable = true;
+  services.dwm-status.order = [ "backlight" "time" "battery"  ];
+  services.dwm-status.extraConfig = ''
+  	debug = false
+	separator = "  |  "
+
+	[audio]
+	control = "Master"
+	mute = "ﱝ"
+	template = "{ICO} {VOL}%"
+	icons = ["奄", "奔", "墳"]
+
+	[backlight]
+	device = "intel_backlight"
+	template = "{ICO} {BL}%"
+	icons = ["", "", ""]
+
+	[battery]
+	charging = ""
+	discharging = ""
+	enable_notifier = true
+	no_battery = ""
+	notifier_critical = 10
+	notifier_levels = [2, 5, 10, 15, 20]
+	separator = " · "
+	icons = ["", "", "", "", "", "", "", "", "", "", ""]
+
+	[cpu_load]
+	template = "{CL1} {CL5} {CL15}"
+	update_interval = 20
+
+	[network]
+	no_value = "NA"
+	template = "{IPv4} · {IPv6} · {ESSID}"
+
+	[time]
+	format = "%d-%m-%Y %H:%M"
+	update_seconds = false
+  '';
 
   nixpkgs.overlays = [
     (final: prev: {
@@ -55,6 +55,7 @@
          ./dwm-systray-6.4.diff
          ./dwm-cool-autostart-6.2.diff
          ./dwm-ru_gaps-6.3.diff
+         ./dwm-inplacerotate-6.2.diff
          # ./dwm-ru_bottomstack-6.2.diff
          # ./dwm-warp-6.2.diff
          ./dwm-warp-6.4.diff
--- home/home.nix
+++ home/home.nix
@@ -4,7 +4,7 @@
   imports = [
    ./zsh/default.nix
    ./dunst/default.nix
-   ./dwm/default.nix
+   # ./dwm/default.nix
    # ./sway/default.nix
    # ./hyprland/default.nix
    ./waybar/default.nix
