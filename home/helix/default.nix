eb7b78ff9ab2a4204fc4e714552a673c789857de
rafimrfdn <uphy.mks@gmail.com>
2024-03-19 18:01:11 +0800

    pisahin set helix di home manager

--- home/helix/default.nix
+++ home/helix/default.nix
@@ -1,23 +1,11 @@
-{config, pkgs, input, ...}:
+{config, pkgs, ...}:
 
 {
   programs.helix = {
     enable = true; 
     settings = 
-    { 
-    theme = "base16_terminal"  
-    [editor]
-    line-number = "relative"
-    rulers = [80]
-    shell = ["zsh", "-c" ]
-
-    [editor.cursor-shape]
-    insert = "bar"
-    normal = "block"
-    select = "underline"
-
-    [editor.file-picker]
-    hidden = false
-    };
-  }
+    '' 
+      ${builtins.readFile ./config.toml}
+    '';
+  };
 }
