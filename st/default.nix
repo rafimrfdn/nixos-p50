b4b7ad4154be41f663401725b168935d4879640c
rafimrfdn <uphy.mks@gmail.com>
2024-11-03 00:12:37 +0800

    update

--- st/default.nix
+++ st/default.nix
@@ -9,12 +9,11 @@ let
 
       # Specify the patches you want to apply
       patches = [
-        # ./patches/st-scrollback-0.8.5.diff
-        ./patches/st-anysize-0.8.4.diff
         ./patches/st-boxdraw_v2-0.8.5.diff
-        # ./patches/st-scrollback-0.9.2.diff
+        ./patches/st-nobadweight-20220308-4b53196.diff
+        ./patches/st-scrollback-0.9.2.diff
         ./patches/st-font2-0.8.5.diff
-        # ./patches/st-vertcenter.diff
+        ./patches/st-vertcenter.diff
         # ./patches/st-glyph-wide-support-20220411-ef05519.diff
         # ./patches/st-charoffsets-20220311-0.8.5.diff
         # ./patches/st-moonfly-0.8.2.diff
