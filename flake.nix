e5db926e08fdaa62f8dd7d37404e782c12303b01
rafimrfdn <uphy.mks@gmail.com>
2025-02-03 23:29:25 +0800

    update

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
     # display-flake.url = "path:./flakes/display";
   };
 
-  outputs = { self, nixpkgs, home-manager, .. }:
+  outputs = { self, nixpkgs, home-manager, ... }:
   let
     system = "x86_64-linux";
   in {
